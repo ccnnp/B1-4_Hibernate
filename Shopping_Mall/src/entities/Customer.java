bb40131468af0510c66e33191728d3fcad98e7f3
Home <Home@LAPTOP-QFF9CGK9>
2022-06-01 17:26:22 +0530

    Second Commit

---
+++ Shopping_Mall/src/entities/Customer.java
@@ -0,0 +1,62 @@
+package entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Customer")
+public class Customer implements Serializable
+{
+	private static final long serialVersionUID=1L;
+	@Id
+	@Column(name="customerId")
+	private int id;
+	@Column(name="customerName")
+	private String name;
+	@Column(name="orderId")
+	private int order_id;
+	@Column(name="Phone")
+	private String phone;
+	@Column(name="email")
+	private String email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getOrder_id() {
+		return order_id;
+	}
+	public void setOrder_id(int order_id) {
+		this.order_id = order_id;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", order_id=" + order_id + ", phone=" + phone + ", email="
+				+ email + "]";
+	}
+	
+}
